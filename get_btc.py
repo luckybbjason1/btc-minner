6e4eafd15260393c11de29ba7f055ba31128e187
liuchuang <326514244@qq.com>
2024-11-30 20:55:14 +0800

    1

--- get_btc.py
+++ get_btc.py
@@ -4,5 +4,5 @@ import time  # 添加 time 模块导入
 if __name__ == "__main__":
     for i in range(10):
         wallet = get_wallet('main')
-        print(wallet)
+        # print(wallet)
         time.sleep(1)  # 每次请求后暂停 1 秒
