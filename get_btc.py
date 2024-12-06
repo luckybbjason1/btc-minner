ce0535db569b2400c8ec523a9ddc9e3f55d9a69c
ericliu <ericlc727@gmail.com>
2024-12-06 17:54:48 +0800

    11

--- get_btc.py
+++ get_btc.py
@@ -2,7 +2,7 @@ from get_wallet import get_wallet
 import time  # 添加 time 模块导入
 
 if __name__ == "__main__":
-    for i in range(10):
+    while(True):
         wallet = get_wallet('main')
         # print(wallet)
         time.sleep(1)  # 每次请求后暂停 1 秒
